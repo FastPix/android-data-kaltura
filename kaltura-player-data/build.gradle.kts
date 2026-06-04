57f89298b7a31663d6e62c9579b6729fa0853fef
Siddharth <siddharth.s@fastpix.io>
2026-06-04 13:20:29 +0530

    - **SDK & Dependencies**     - Bumped `SDK_VERSION` to `1.0.4` in `FastPixKalturaLibraryInfo.kt`.     - Updated publication version to `1.0.4` in `build.gradle.kts`.     - Upgraded `core` library dependency to `1.3.1` in `libs.versions.toml`. - **Documentation**     - Updated installation instructions in `README.md` to reference version `1.0.4`.     - Added release notes for `v1.0.4` in `CHANGELOG.MD`.

--- kaltura-player-data/build.gradle.kts
+++ kaltura-player-data/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         create<MavenPublication>("bar") {
             groupId = "io.fastpix.data"
             artifactId = "kaltura"
-            version = "1.0.3"
+            version = "1.0.4"
             artifact("${buildDir}/outputs/aar/kaltura-player-data-release.aar")
 
             pom.withXml {
